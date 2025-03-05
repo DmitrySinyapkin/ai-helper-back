c7809bcb198596c29340ab30e70e646ad21caf65
DmitrySinyapkin <dsinyapkin@mail.ru>
2025-03-05 16:44:38 +0300

    added cors package and .env.example

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import 'dotenv/config'
+import cors from 'cors'
 import authRoutes from './routes/authRoutes'
 import chatRoutes from './routes/chatRoutes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
