c1df852eab6fe00b86ed551163c1bc17192fb338
DmitrySinyapkin <dsinyapkin@mail.ru>
2025-03-07 18:43:08 +0300

    fixed type errors in jwt utils

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,16 +1,19 @@
-import jwt from 'jsonwebtoken'
+import jwt, { SignOptions } from 'jsonwebtoken'
 import { User } from '../types/user'
 
 const JWT_SECRET = process.env.JWT_SECRET!
 const REFRESH_SECRET = process.env.REFRESH_SECRET!
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN! || '1h'
-const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN! || '7d'
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN ? Number(process.env.JWT_EXPIRES_IN) : 3600
+const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN ? Number(process.env.REFRESH_EXPIRES_IN) : 604800
 
-export const generateAccessToken = (user: Pick<User, 'id' | 'email'>) =>
-  jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN })
+const generateToken = (user: Pick<User, 'id' | 'email'>, secret: string, expiresIn: number) => {
+  const options: SignOptions = { expiresIn }
+  return jwt.sign({ id: user.id, email: user.email }, secret, options)
+}
 
-export const generateRefreshToken = (user: Pick<User, 'id' | 'email'>) =>
-  jwt.sign({ id: user.id, email: user.email }, REFRESH_SECRET, { expiresIn: REFRESH_EXPIRES_IN })
+export const generateAccessToken = (user: Pick<User, 'id' | 'email'>) => generateToken(user, JWT_SECRET, JWT_EXPIRES_IN) 
+
+export const generateRefreshToken = (user: Pick<User, 'id' | 'email'>) => generateToken(user, REFRESH_SECRET, REFRESH_EXPIRES_IN)
 
 const verifyToken = (token: string, secret: string) => {
     try {
